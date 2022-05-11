fb1241ba21b6def1ba7b86cfdb8d8da909f6f7a9
Om Prakash Sharma <om@habilelabs.io>
2022-05-11 11:17:57 +0530

    - update code

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -5,16 +5,15 @@ import { IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = ({ title = 'Header' }) => {
-    return (
-        <header style={header}>
-            <label style={menu} htmlFor="icon-button-file">
-                <IconButton color="primary" aria-label="menu" component="span">
-                    <MenuIcon />
-                </IconButton>
-            </label>
-            {title}
-        </header>
-    )
+    return <header style={header}>
+        <label style={menu} htmlFor="icon-button-file">
+            <IconButton color="primary" aria-label="menu" component="span">
+                <MenuIcon />
+            </IconButton>
+        </label>
+        {title}
+    </header>
+
 }
 
 export default Header;
